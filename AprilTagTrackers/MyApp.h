f7c6cd8961af776e77114533e6e48b8d5af89e62
Rasmus Göransson <rasmus@volumental.com>
2021-02-25 20:30:31 +0100

    MyApp: Add override specifiers for wx methods

--- AprilTagTrackers/MyApp.h
+++ AprilTagTrackers/MyApp.h
@@ -18,8 +18,8 @@ class MyApp : public wxApp
     GUI* gui;
 
 public:
-    virtual int OnExit();
-    virtual bool OnInit();
+    virtual int OnExit() wxOVERRIDE;
+    virtual bool OnInit() wxOVERRIDE;
     void ButtonPressedCamera(wxCommandEvent&);
     void ButtonPressedCameraCalib(wxCommandEvent&);
     void ButtonPressedCameraPreview(wxCommandEvent&);
