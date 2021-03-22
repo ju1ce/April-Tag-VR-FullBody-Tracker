3faf623e778cfdec88f0a849e1263b8c9ef191cd
Rasmus Göransson <rasmus@volumental.com>
2021-03-22 22:23:30 +0100

    Remove obsolete comment in Tracker.h

--- AprilTagTrackers/Tracker.h
+++ AprilTagTrackers/Tracker.h
@@ -1,7 +1,4 @@
-﻿// AprilTagTrackers.h : Include file for standard system include files,
-// or project specific include files.
-
-#pragma once
+﻿#pragma once
 
 #include <iostream>
 #include <mutex>
