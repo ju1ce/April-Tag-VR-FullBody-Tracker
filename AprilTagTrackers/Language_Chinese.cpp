5b73ccf9d94a64b47fce05ddfcb77ec67993f3b1
ApriltagTrackers 翻译员 <91396010+apriltagtrackers-cn@users.noreply.github.com>
2021-10-17 08:50:18 -0400

    Update Language_Chinese.cpp
    add translation for DISABLE_OUT button, and reduce the length of some text to fit the GUI

--- AprilTagTrackers/Language_Chinese.cpp
+++ AprilTagTrackers/Language_Chinese.cpp
@@ -26,9 +26,10 @@ Lang get_lang_chinese()
 	language.CAMERA_PREVIEW_CAMERA = "查看摄像头画面";
 	language.CAMERA_PREVIEW_CALIBRATION = "查看校准数据";
 	language.CAMERA_CALIBRATION_MODE = "位置校准模式";
-	language.CAMERA_MULTICAM_CALIB = "将当前实例的位置对齐到其它实例的位置";
+	language.CAMERA_MULTICAM_CALIB = "将当前实例的白色摄像头与其它实例的白色摄像头对齐位置";
 	language.CAMERA_LOCK_HEIGHT = "在使用手柄校准时锁定Y轴";
-	language.CAMERA_CALIBRATION_INSTRUCTION = "禁用 SteamVR 家并退出游戏以看到白色相机。\n使用左手柄的扳机抓住白色相机，使其与现实生活中的摄像头处于相同位置，然后使用左手柄的抓握键旋转白色相机，使其与现实生活中的摄像头处于相同朝向。\n完成校准后取消勾选\"位置校准模式\"以保存校准数据！\n\n\n";
+	language.CAMERA_CALIBRATION_INSTRUCTION = "禁用 SteamVR 家并退出游戏以看到白色摄像头。\n使用左手柄的扳机抓住白色摄像头，使其与现实生活中的摄像头处于相同位置，然后使用左手柄的抓握键旋转白色摄像头，使其与现实生活中的摄像头处于相同朝向。\n完成校准后取消勾选 \"位置校准模式\" 以保存校准数据！\n\n\n";
+	language.CAMERA_DISABLE_OUT = "关闭输出窗口";
 
 	language.cameraApiDescriptions = []()
 	{
@@ -65,8 +66,8 @@ Lang get_lang_chinese()
 	language.PARAMS_CAMERA_TOOLTIP_FPS = "尝试设置摄像头的 FPS";
 	language.PARAMS_CAMERA_NAME_SETTINGS = "尝试打开摄像头菜单";
 	language.PARAMS_CAMERA_TOOLTIP_SETTINGS = "有些摄像头可以勾选此项以打开摄像头菜单。这个选项仅在视频捕获 API 为 DirectShow（700）时才会生效";
-	language.PARAMS_CAMERA_NAME_3_OPTIONS = "尝试为摄像头配置后面的三个参数";
-	language.PARAMS_CAMERA_TOOLTIP_3_OPTIONS = "实验性功能。勾选此项将禁用摄像头的自动对焦，并启用后面的三个参数。";
+	language.PARAMS_CAMERA_NAME_3_OPTIONS = "尝试配置后面的三个参数";
+	language.PARAMS_CAMERA_TOOLTIP_3_OPTIONS = "实验性功能。勾选此项将禁用摄像头的自动对焦，并尝试为摄像头配置后面的三个参数。";
 	language.PARAMS_CAMERA_NAME_AUTOEXP = "摄像头自动曝光";
 	language.PARAMS_CAMERA_TOOLTIP_AUTOEXP = "实验性功能。将尝试设置摄像头的自动曝光功能。大部分 USB 摄像头填写 1 将启用自动曝光， 填写 0 将禁用自动曝光， 但也有些摄像头是 0.75 与 0.25";
 	language.PARAMS_CAMERA_NAME_EXP = "摄像头快门速度";
@@ -77,16 +78,16 @@ Lang get_lang_chinese()
 	language.PARAMS_TRACKER = "追踪器";
 	language.PARAMS_TRACKER_NAME_NUM_TRACKERS = "追踪器的数量";
 	language.PARAMS_TRACKER_TOOLTIP_NUM_TRACKERS = "设置为 3 才能在 VRchat 得到全身追踪！";
-	language.PARAMS_TRACKER_NAME_MARKER_SIZE = "边界长度";
-	language.PARAMS_TRACKER_TOOLTIP_MARKER_SIZE = "\"边界\" 是一个正方形在白色方框与黑块方框的相邻处，测量它的边长并填写到这里。";
+	language.PARAMS_TRACKER_NAME_MARKER_SIZE = "边界长度（厘米）";
+	language.PARAMS_TRACKER_TOOLTIP_MARKER_SIZE = "\"边界\" 是一个正方形在白色方框与黑块方框的相邻处，以毫米级精度（精确到小数点后一位）测量它的边长并填写到这里。";
 	language.PARAMS_TRACKER_NAME_QUAD_DECIMATE = "欠采样倍率";
 	language.PARAMS_TRACKER_TOOLTIP_QUAD_DECIMATE = "它可以是 1、1.5、2、3、4 这样的数字。较大的数字将减少每帧所需要的 CPU 时间并优化性能，但会减少二维码被检测到的概率。";
 	language.PARAMS_TRACKER_NAME_SEARCH_WINDOW = "搜索范围";
 	language.PARAMS_TRACKER_TOOLTIP_SEARCH_WINDOW = "搜索范围的大小。较小的搜索范围将减少每帧所需要的 CPU 时间并优化性能，但会在运动模糊产生时丢失追踪并难以再次找回追踪。";
 	language.PARAMS_TRACKER_NAME_MARKER_LIBRARY = "二维码类型";
 	language.PARAMS_TRACKER_TOOLTIP_MARKER_LIBRARY = "切换要使用的二维码类型。仅限开发者使用，默认选项为 ApriltagStandard。";
-	language.PARAMS_TRACKER_NAME_USE_CENTERS = "在每组二维码的中心点生成追踪器";
-	language.PARAMS_TRACKER_TOOLTIP_USE_CENTERS = "实验性功能。启用此选项以在每组二维码的中心点生成追踪器，而不是在主二维码上生成追踪器。";
+	language.PARAMS_TRACKER_NAME_USE_CENTERS = "追踪器居中生成";
+	language.PARAMS_TRACKER_TOOLTIP_USE_CENTERS = "实验性功能。启用此选项以在每组二维码的中央生成追踪器，而不是在主二维码上生成追踪器。";
 	language.PARAMS_TRACKER_NAME_IGNORE_0 = "忽略第一个追踪器";
 	language.PARAMS_TRACKER_TOOLTIP_IGNORE_0 = "如果想用 vive tracker 或 owotracker 替换掉腰部追踪器，勾选它并保持追踪器的数量为 3。";
 
@@ -94,7 +95,7 @@ Lang get_lang_chinese()
 	language.PARAMS_SMOOTHING_NAME_WINDOW = "时间窗口";
 	language.PARAMS_SMOOTHING_TOOLTIP_WINDOW = "此时间窗口中的样本将用于线性插值的计算。以秒为单位，越大抖动越少，但会增加延迟并造成更多的粘滞感。最佳值通常处于 0.2 至 0.5 之间。";
 	language.PARAMS_SMOOTHING_NAME_ADDITIONAL = "额外平滑";
-	language.PARAMS_SMOOTHING_TOOLTIP_ADDITIONAL = "对追踪器的定位结果应用额外的平滑处理算法。将模仿 0.4 版本中的平滑算法。0 的意思是禁用，1 是最大值。";
+	language.PARAMS_SMOOTHING_TOOLTIP_ADDITIONAL = "对追踪器的定位结果应用额外的平滑算法。将模仿 0.4 版本中的平滑算法。0 的意思是禁用，1 是最大值。";
 	language.PARAMS_SMOOTHING_NAME_DEPTH = "深度平滑";
 	language.PARAMS_SMOOTHING_TOOLTIP_DEPTH = "实验性功能。对深度估算的结果应用额外的平滑处理算法，之所以要这样做是因为深度信息拥有更高的误差。以极少的延迟作为代价将不规则抖动稀释为匀速运动。（ 0 表示禁用，建议值为 0 至 1 之间 ）";
 	language.PARAMS_SMOOTHING_NAME_CAM_LATENCY = "摄像头延迟（latency）";
@@ -106,8 +107,8 @@ Lang get_lang_chinese()
 	language.PARAMS_NOTE_LOW_SMOOTHING = "提示：平滑器的时间窗口太小，这可能会导致问题。\n\n如果你的追踪器出现任何问题，请尝试增加它。";
 	language.PARAMS_NOTE_QUAD_NONSTANDARD = "提示：欠采样倍率不是标准值。\n\n保持它为 1、1.5、2、3、4，否则可能会导致追踪器不被定位。";
 	language.PARAMS_NOTE_NO_DSHOW_CAMSETTINGS = "提示：启用了\"尝试打开摄像头菜单\"，但视频捕获 API 不是700。\n\n\"尝试打开摄像头菜单\"功能仅在视频捕获 API 为 DirectShow （700）时有效。";
-	language.PARAMS_NOTE_LATENCY_GREATER_SMOOTHING = "提示：摄像头延迟（latency）的值永远不应高于额外平滑的值，否则追踪器不会被定位。\n\n您也许应在 摄像头延迟（latency）尝试 0.1 以下的值。";
-	language.PARAMS_NOTE_HIGH_SMOOTHING = "提示：额外平滑的值大于 1，这将导致很高的延迟！\n\n您也许应尝试像是 0.5 这样的值。";
+	language.PARAMS_NOTE_LATENCY_GREATER_SMOOTHING = "提示：摄像头延迟（latency）的值永远不应高于 \"时间窗口\" 的值，否则追踪器不会被定位。\n\n您也许应在 摄像头延迟（latency）尝试 0.1 以下的值。除非您的摄像头拥有很快的拍摄速度，否则不建议在 \"时间窗口\" 使用0.2以下的值。";
+	language.PARAMS_NOTE_HIGH_SMOOTHING = "提示：\"时间窗口\" 的值大于 1，这将导致很高的延迟！\n\n您也许应尝试像是 0.5 这样的值。";
 	language.PARAMS_NOTE_2TRACKERS_IGNORE0 = "追踪器数量为 2 但却启用了 \"忽略第一个追踪器\"。这将导致 SteamVR 中只显示一个腰部追踪器。\n如果您想同时使用双脚上的追踪器，需要将追踪器数量设置为 3。";
 	language.PARAMS_NOTE_LANGUAGECHANGE = L"语言已更改！请重启这个软件以应用最新设置。（简体中文）";
 
@@ -124,11 +125,11 @@ Lang get_lang_chinese()
 	language.TRACKER_CAMERA_NOTRUNNING = "先启动摄像头才能使用此功能。";
 
 	language.TRACKER_CAMERA_CALIBRATION_INSTRUCTIONS = "开始校准摄像头！\n\n"
-		"将打印好的 charuco_board 放在平坦的表面上，或直接在（非曲面）显示器上显示 charuco_board。摄像头将拍摄一些 charuco_board 的照片，从尽可能多的不同角度和距离拍摄 charuco_board 的图片。\n\n"
+		"将打印好的 charuco_board 放在平坦的表面上，或直接在（非曲面）显示器上显示 charuco_board。摄像头将自动拍摄一些 charuco_board 的图像，从尽可能多的不同角度和距离拍摄 charuco_board 的图像。\n\n"
 		"确保 charuco_board 清晰可见，并缓慢的移动您的摄像头。\n\n"
 		"紫点 = 优秀\n绿点 = 一般\n黄点 = 很差\n\n"
-		"这些网格应该笔直，平坦。并铺满整个图像。网格需要保持稳定，但可以在几个像素之间有轻微的抖动。\n"
-		"这些点应该均匀地分布在整个图像上。\n\n"
+		"这些网格应该笔直，平坦，并铺满整个画面。网格需要保持稳定，但可以在几个像素的范围内有轻微的抖动。\n"
+		"这些彩色的点应该均匀地分布在整个画面上。\n\n"
 		"当您对校准结果感到满意时，按下确定按钮以保存校准数据。";
 
 	language.TRACKER_CAMERA_CALIBRATION_COMPLETE = "校准成功，数据已保存。";
