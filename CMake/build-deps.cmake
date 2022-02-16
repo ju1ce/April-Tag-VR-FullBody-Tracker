5e634c6a163958f9b7ceb8a90e0604310f8e7d54
Funnbot <22226942+funnbot@users.noreply.github.com>
2022-02-16 00:54:05 -0800

    More build fixes

--- CMake/build-deps.cmake
+++ CMake/build-deps.cmake
@@ -1,10 +1,6 @@
 function(build_deps config)
 
-if (${DEPS_BUILD_FAILED})
-    return()
-endif()
-
-message(STATUS "Building deps: ${config}")
+message(STATUS "Build deps: ${config}")
 
 if (DEFINED CMAKE_CONFIGURATION_TYPES)
     set(DEPS_BUILD_MULTI --config ${config})
@@ -12,33 +8,39 @@ else()
     set(DEPS_BUILD_SINGLE -DCMAKE_BUILD_TYPE:STRING=${config})
 endif()
 
+make_directory("${DEPS_BINARY_DIR}")
+
 execute_process(
-    COMMAND ${CMAKE_COMMAND} -B "${DEPS_BINARY_DIR}" -S "${DEPS_PREFIX}"
+    COMMAND ${CMAKE_COMMAND} -B "${DEPS_BINARY_DIR}" -S "${DEPS_PREFIX}" -G "${CMAKE_GENERATOR}"
         ${DEPS_BUILD_SINGLE}
         -DCMAKE_INSTALL_PREFIX:PATH=${DEPS_INSTALL_DIR}
         -DBUILD_SHARED:BOOL=${BUILD_DEPS_SHARED}
+    OUTPUT_FILE "${DEPS_BINARY_DIR}/deps_build.log"
     RESULT_VARIABLE DEPS_BUILD_RESULT)
 
 if (NOT ${DEPS_BUILD_RESULT} EQUAL 0)
-    set(DEPS_BUILD_FAILED TRUE PARENT_SCOPE)
-    return()
+    message(FATAL_ERROR "Failed to configure deps: \nError: ${DEPS_BUILD_RESULT}")
 endif()
+message(STATUS "Configure done.")
 
 execute_process(
-    COMMAND ${CMAKE_COMMAND} --build "${DEPS_BINARY_DIR}" ${DEPS_BUILD_MULTI} --target AprilTagTracker
+    COMMAND ${CMAKE_COMMAND} --build "${DEPS_BINARY_DIR}" ${DEPS_BUILD_MULTI} --target AprilTagTrackers
+    OUTPUT_FILE "${DEPS_BINARY_DIR}/deps_build.log"
     RESULT_VARIABLE DEPS_BUILD_RESULT)
 
 if (NOT ${DEPS_BUILD_RESULT} EQUAL 0)
-    set(DEPS_BUILD_FAILED TRUE PARENT_SCOPE)
-    return()
+    message(FATAL_ERROR "Failed to build deps: \nError: ${DEPS_BUILD_RESULT}")
 endif()
+message(STATUS "Build done.")
 
 execute_process(
     COMMAND ${CMAKE_COMMAND} --install "${DEPS_BINARY_DIR}" ${DEPS_BUILD_MULTI} --prefix "${DEPS_INSTALL_DIR}"
+    OUTPUT_FILE "${DEPS_BINARY_DIR}/deps_build.log"
     RESULT_VARIABLE DEPS_BUILD_RESULT)
 
 if (NOT ${DEPS_BUILD_RESULT} EQUAL 0)
-    set(DEPS_BUILD_FAILED TRUE PARENT_SCOPE)
+    message(FATAL_ERROR "Failed to install deps: \nError: ${DEPS_BUILD_RESULT}")
 endif()
+message(STATUS "Install done.")
 
 endfunction()
