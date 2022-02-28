86467dc0b8e84a89c3effdc3a603d20a606b1bf8
Funnbot <22226942+funnbot@users.noreply.github.com>
2022-02-27 20:54:30 -0800

    moved platform defines to cmake

--- AprilTagTrackers/Util.h
+++ AprilTagTrackers/Util.h
@@ -1,16 +1,5 @@
 #pragma once
 
-#if defined(WIN32) || defined(_WIN32) || defined(__WIN32)
-    #define OS_WIN 1
-#elif defined(unix) || defined(__unix__)
-    #define OS_LINUX 1
-#elif defined(__APPLE__)
-    #define OS_MACOS 1
-    #error Building on MacOS is not currently supported
-#else
-    #error Unknown OS
-#endif
-
 #include <string>
 
 void sleep_millis(int);
