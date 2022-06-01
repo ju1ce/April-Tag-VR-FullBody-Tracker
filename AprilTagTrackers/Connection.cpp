df785a507d4d9db6fce83f088a956086b3518fb8
Funnbot <22226942+funnbot@users.noreply.github.com>
2022-06-01 12:26:01 -0700

    Fix getting controller input

--- AprilTagTrackers/Connection.cpp
+++ AprilTagTrackers/Connection.cpp
@@ -282,7 +282,7 @@ int Connection::GetButtonStates()
         return 0;
     }
 
-    vr::VRActiveActionSet_t actionSet;
+    vr::VRActiveActionSet_t actionSet{};
     actionSet.ulActionSet = m_actionsetDemo;
     vr::VRInput()->UpdateActionState(&actionSet, sizeof(actionSet), 1);
 
@@ -357,7 +357,7 @@ Pose Connection::GetControllerPose()
             matrix.m[0][0], matrix.m[0][1], matrix.m[0][2],
             matrix.m[1][0], matrix.m[1][1], matrix.m[1][2],
             matrix.m[2][0], matrix.m[2][1], matrix.m[2][2]);
-        cv::Quatd rot = cv::Quatd::createFromRotMat(rotMat);
+        cv::Quatd rot = cv::Quatd::createFromRotMat(rotMat).normalize();
 
         cv::Vec3d pos{matrix.m[0][3], matrix.m[1][3], matrix.m[2][3]};
 
