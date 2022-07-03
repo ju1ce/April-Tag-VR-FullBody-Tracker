3b0a186f01adeb2b29c931196f832bf8edeb5783
Funnbot <22226942+funnbot@users.noreply.github.com>
2022-07-02 19:12:36 -0700

    update linkage in triplet

--- triplets/x64-windows-static.cmake
+++ triplets/x64-windows-static.cmake
@@ -7,11 +7,9 @@ set(VCPKG_LIBRARY_LINKAGE static)
 #set(VCPKG_PLATFORM_TOOLSET "v143")
 
 if (PORT STREQUAL "openvr")
-    set(VCPKG_CRT_LINKAGE dynamic)
     set(VCPKG_LIBRARY_LINKAGE dynamic)
 endif()
 
-if (PORT STREQUAL "wxwidgets")
-    set(WXWIDGETS_USE_STL TRUE)
-    set(WXWIDGETS_USE_STD_CONTAINERS TRUE)
+if (PORT STREQUAL "apriltag")
+    set(VCPKG_LIBRARY_LINKAGE static)
 endif()
