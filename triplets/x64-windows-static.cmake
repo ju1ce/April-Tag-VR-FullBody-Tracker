837d6bfd03c1d7a6f879df593a7ce45d0431cace
Funnbot <22226942+funnbot@users.noreply.github.com>
2022-07-02 18:02:21 -0700

    update triplet for windows static

--- triplets/x64-windows-static.cmake
+++ triplets/x64-windows-static.cmake
@@ -1,19 +1,14 @@
 set(ENV{VCPKG_DISABLE_METRICS} ON)
 
 set(VCPKG_TARGET_ARCHITECTURE x64)
-set(VCPKG_CRT_LINKAGE static)
+set(VCPKG_CRT_LINKAGE dynamic)
 set(VCPKG_LIBRARY_LINKAGE static)
 
-set(VCPKG_PLATFORM_TOOLSET "v143")
+#set(VCPKG_PLATFORM_TOOLSET "v143")
 
 if (PORT STREQUAL "openvr")
     set(VCPKG_CRT_LINKAGE dynamic)
     set(VCPKG_LIBRARY_LINKAGE dynamic)
-endif()
-
-if (PORT STREQUAL "opencv4")
-    set(DBUILD_LIST "aruco,videoio,objdetect,highgui,calib3d" CACHE STRING "" FORCE)
-    set(VIDEOIO_PLUGIN_LIST "ffmpeg,gstreamer" CACHE STRING "" FORCE)
 endif()
 
 if (PORT STREQUAL "wxwidgets")
