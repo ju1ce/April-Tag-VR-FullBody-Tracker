de85521d697e1f758a671f0ce7401578df56d7de
Funnbot <22226942+funnbot@users.noreply.github.com>
2022-07-03 00:16:33 -0700

    fix apriltag vcpkg port config fixup was using incorrect path

--- vcpkg-ports/apriltag/portfile.cmake
+++ vcpkg-ports/apriltag/portfile.cmake
@@ -58,7 +58,7 @@ vcpkg_cmake_configure(
 
 vcpkg_cmake_install()
 
-vcpkg_cmake_config_fixup()
+vcpkg_cmake_config_fixup(CONFIG_PATH "share/${PORT}/cmake")
 vcpkg_fixup_pkgconfig()
 vcpkg_copy_pdbs()
 
