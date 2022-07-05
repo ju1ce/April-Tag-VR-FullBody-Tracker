8dbab08a5205140bb86d43b7902e4e788b6b3fd5
Funnbot <22226942+funnbot@users.noreply.github.com>
2022-07-04 17:51:30 -0700

    build: vcpkg openvr port remove debug/share

--- vcpkg-ports/openvr/portfile.cmake
+++ vcpkg-ports/openvr/portfile.cmake
@@ -52,5 +52,6 @@ vcpkg_fixup_pkgconfig()
 vcpkg_copy_pdbs()
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
 
 file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
