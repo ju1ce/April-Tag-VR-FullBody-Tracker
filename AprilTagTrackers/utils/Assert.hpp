defd71ca0ab03eb9eeec9bf143f85e433b48051f
Funnbot <22226942+funnbot@users.noreply.github.com>
2022-07-26 18:33:34 -0700

    fix: ATT_FATAL_EXCEPTION will abort if ATT_DEBUG isnt defined

--- AprilTagTrackers/utils/Assert.hpp
+++ AprilTagTrackers/utils/Assert.hpp
@@ -18,23 +18,27 @@
 #    define ATT_ASSERT(p_expr, ...) ATT_NOOP()
 #endif
 
-#define ATT_FATAL_EXCEPTION(p_throwExpr, p_context)            \
-    do                                                         \
-    {                                                          \
-        try                                                    \
-        {                                                      \
-            (p_throwExpr);                                     \
-        }                                                      \
-        catch (const std::exception& exc)                      \
-        {                                                      \
-            ATT_LOG_ERROR(p_context, ": ", exc.what());        \
-            ATT_ABORT();                                       \
-        }                                                      \
-        catch (...)                                            \
-        {                                                      \
-            ATT_LOG_ERROR(p_context, ": malformed exception"); \
-            ATT_ABORT();                                       \
-        }                                                      \
-        ATT_LOG_ERROR(p_context, ": expected exception");      \
-        ATT_ABORT();                                           \
-    } while (false)
+#ifdef ATT_DEBUG
+#    define ATT_FATAL_EXCEPTION(p_throwExpr, p_context)            \
+        do                                                         \
+        {                                                          \
+            try                                                    \
+            {                                                      \
+                (p_throwExpr);                                     \
+            }                                                      \
+            catch (const std::exception& exc)                      \
+            {                                                      \
+                ATT_LOG_ERROR(p_context, ": ", exc.what());        \
+                ATT_ABORT();                                       \
+            }                                                      \
+            catch (...)                                            \
+            {                                                      \
+                ATT_LOG_ERROR(p_context, ": malformed exception"); \
+                ATT_ABORT();                                       \
+            }                                                      \
+            ATT_LOG_ERROR(p_context, ": expected exception");      \
+            ATT_ABORT();                                           \
+        } while (false)
+#else
+#    define ATT_FATAL_EXCEPTION(p_throwExpr, p_context) ATT_ABORT();
+#endif
