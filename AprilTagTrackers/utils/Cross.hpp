9212f224094c41503dac7833ae16d5257dad60f3
Funnbot <22226942+funnbot@users.noreply.github.com>
2022-09-09 21:53:19 -0700

    fix: ATT_ABORT was not considering noreturn in debug builds, because breakpoints can be continued from, added abort after breakpoint.

--- AprilTagTrackers/utils/Cross.hpp
+++ AprilTagTrackers/utils/Cross.hpp
@@ -2,9 +2,9 @@
 
 #ifdef ATT_DEBUG
 #    include "Test.hpp" // IWYU pragma: keep
-#else
-#    include <cstdlib> // IWYU pragma: keep
 #endif
+
+#include <cstdlib> // IWYU pragma: keep
 
 // function signature
 #if defined(ATT_COMP_CLANG) || defined(ATT_COMP_GCC)
@@ -15,15 +15,21 @@
 #    define ATT_PRETTY_FUNCTION "<unknown function>()"
 #endif
 
-// debug break or abort
+// trigger a debugger breakpoint
+// on linux this is a statement, and some debuggers cannot resume from it
+#define ATT_DEBUG_BREAK() DOCTEST_BREAK_INTO_DEBUGGER()
+
+// wrap ATT_DEBUG_BREAK in lambda on linux to make it an expression
+// adds an extra lambda to the callstack, but the location will be correct
+#ifdef ATT_COMP_MSVC
+#    define ATT_DEBUG_BREAK_EXPR() ATT_DEBUG_BREAK()
+#else
+#    define ATT_DEBUG_BREAK_EXPR() [] { ATT_DEBUG_BREAK(); }()
+#endif
+
+// call std::abort
 #ifdef ATT_DEBUG
-#    ifdef ATT_COMP_MSVC
-#        define ATT_ABORT() DOCTEST_BREAK_INTO_DEBUGGER()
-#    else
-#        define ATT_ABORT() [] {           \
-            DOCTEST_BREAK_INTO_DEBUGGER(); \
-        }()
-#    endif
+#    define ATT_ABORT() static_cast<void>(ATT_DEBUG_BREAK_EXPR(), std::abort())
 #else
 #    define ATT_ABORT() std::abort()
 #endif
@@ -38,15 +44,15 @@
             ;
 #endif
 
+namespace utils
+{
+
+[[noreturn]] inline void Unreachable() noexcept
+{
 #ifdef ATT_DEBUG
-#    define ATT_UNREACHABLE()         \
-        do {                          \
-            ATT_ABORT();              \
-            ATT_DETAIL_UNREACHABLE(); \
-        } while (false)
-#else
-#    define ATT_UNREACHABLE()         \
-        do {                          \
-            ATT_DETAIL_UNREACHABLE(); \
-        } while (false)
+    ATT_DEBUG_BREAK();
 #endif
+    ATT_DETAIL_UNREACHABLE();
+}
+
+} // namespace utils
