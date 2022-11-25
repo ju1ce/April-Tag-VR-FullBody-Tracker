14e7e4986232abe96f76d75d235318d14d6f4215
Funnbot <22226942+funnbot@users.noreply.github.com>
2022-11-24 17:16:05 -0800

    feat: support for new updatepreview function

--- AprilTagTrackers/GUI/MainFrame.cpp
+++ AprilTagTrackers/GUI/MainFrame.cpp
@@ -96,6 +96,12 @@ void GUI::MainFrame::UpdatePreview(const cv::Mat& image, PreviewId id)
     previews[idx].UpdateImage(image);
 }
 
+void GUI::MainFrame::UpdatePreview(const cv::Mat& image, int constrainSize, PreviewId id)
+{
+    int idx = static_cast<int>(id);
+    previews[idx].UpdateImage(image, constrainSize);
+}
+
 bool GUI::MainFrame::IsPreviewVisible(PreviewId id)
 {
     int idx = static_cast<int>(id);
