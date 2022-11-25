753b643e6ff8e3c876063a636a93723aa8e92e03
Funnbot <22226942+funnbot@users.noreply.github.com>
2022-11-24 17:01:44 -0800

    feat: concepts helpers

---
+++ AprilTagTrackers/utils/Concepts.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <concepts> // IWYU pragma: keep
+
+namespace utils
+{
+
+template <typename T, typename TResult, typename... TArgs>
+concept Callable = std::is_invocable_v<T, TArgs...>;
+template <typename T, typename TResult, typename... TArgs>
+concept CallableR = std::is_invocable_r_v<TResult, T, TArgs...>;
+
+template <typename T1, typename T2>
+concept DecayAs = std::same_as<std::decay_t<T1>, std::decay_t<T2>>;
+
+template <typename TValue, typename... TArgs>
+concept ArgsDecayAs = ((DecayAs<TValue, TArgs>) && ...);
+template <typename TValue, typename... TArgs>
+concept ArgsSameAs = ((std::same_as<TValue, TArgs>) && ...);
+
+} // namespace utils
